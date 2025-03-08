d31d4d3ad94ebd7f6a89be93224e38c90fbc3b5e
Olewwwka <netvinkakk@gmail.com>
2025-03-08 21:35:09 +0300

    categories repository

---
+++ ECommerce.Infrastructure/Interfaces/ICategoriesRepository.cs
@@ -0,0 +1,13 @@
+﻿using ECommerce.Infrastructure.Entities;
+
+namespace ECommerce.Infrastructure.Interfaces
+{
+    public interface ICategoriesRepository
+    {
+        Task<int> Create(CategoryEntity category);
+        Task<int> Delete(int id);
+        Task<List<CategoryEntity>> GetAll();
+        Task<CategoryEntity?> GetByIdAsync(int id);
+        Task<int> Update(int id, string name, string description);
+    }
+}
