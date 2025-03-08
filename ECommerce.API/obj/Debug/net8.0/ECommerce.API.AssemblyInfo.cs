af182b2ba50a7cc22a5f85b4026fcf2efb2810b3
Olewwwka <netvinkakk@gmail.com>
2025-03-08 17:48:15 +0300

    creating structure of project

--- ECommerce.API/obj/Debug/net8.0/ECommerce.API.AssemblyInfo.cs
+++ ECommerce.API/obj/Debug/net8.0/ECommerce.API.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerce.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08b869827cf5d92e04824bf93f3dc64ae73620bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerce.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerce.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
