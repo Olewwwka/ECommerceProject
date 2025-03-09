59fbeef8aff530182d1f1470700f5ea81462442e
Olewwwka <netvinkakk@gmail.com>
2025-03-09 14:57:03 +0300

    creating users mapping

---
+++ ECommerce.Infrastructure/Mapping/UserProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using ECommerce.Core.Models;
+using ECommerce.Infrastructure.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Infrastructure.Mapping
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<User, UserEntity>()
+                .ForMember(dest => dest.UserId, opt => opt.Ignore()) 
+                .ForMember(dest => dest.PasswordHash, opt => opt.MapFrom(src => src.PasswordHash));
+        }
+    }
+}
