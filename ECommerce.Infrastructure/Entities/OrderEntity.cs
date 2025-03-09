0de077ecdc87303f8525f5eb73d1aff3fc2ebac7
Olewwwka <netvinkakk@gmail.com>
2025-03-09 14:57:45 +0300

    updating primary keys

--- ECommerce.Infrastructure/Entities/OrderEntity.cs
+++ ECommerce.Infrastructure/Entities/OrderEntity.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +11,8 @@ namespace ECommerce.Infrastructure.Entities
 {
     public class OrderEntity
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int OrderId { get; set; }
         public int UserId { get; set; }
         public decimal TotalAmount { get; set; } = 0;
--- ECommerce.Infrastructure/Entities/ProductEntity.cs
+++ ECommerce.Infrastructure/Entities/ProductEntity.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +11,8 @@ namespace ECommerce.Infrastructure.Entities
 {
     public class ProductEntity
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ProductId { get; set; }
         public int CategoryId { get; set; }
         public CategoryEntity Category { get; set; }
